69cfe0d66cc4d1ef5a84a24bbf3ad7eeffbfb5e4
Luca Burgazzoli <lburgazzoli@gmail.com>
2022-05-25 18:01:23 +0200

    rhoc: update to latest APIs

--- rhoc/pkg/api/admin/api_connector_clusters_admin.go
+++ rhoc/pkg/api/admin/api_connector_clusters_admin.go
@@ -929,6 +929,126 @@ func (a *ConnectorClustersAdminApiService) GetConnectorExecute(r ApiGetConnector
 	return localVarReturnValue, localVarHTTPResponse, nil
 }
 
+type ApiGetConnectorClusterRequest struct {
+	ctx                context.Context
+	ApiService         *ConnectorClustersAdminApiService
+	connectorClusterId string
+}
+
+func (r ApiGetConnectorClusterRequest) Execute() (*ConnectorCluster, *http.Response, error) {
+	return r.ApiService.GetConnectorClusterExecute(r)
+}
+
+/*
+GetConnectorCluster Get a connector cluster
+
+ @param ctx context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
+ @param connectorClusterId The id of the connector cluster
+ @return ApiGetConnectorClusterRequest
+*/
+func (a *ConnectorClustersAdminApiService) GetConnectorCluster(ctx context.Context, connectorClusterId string) ApiGetConnectorClusterRequest {
+	return ApiGetConnectorClusterRequest{
+		ApiService:         a,
+		ctx:                ctx,
+		connectorClusterId: connectorClusterId,
+	}
+}
+
+// Execute executes the request
+//  @return ConnectorCluster
+func (a *ConnectorClustersAdminApiService) GetConnectorClusterExecute(r ApiGetConnectorClusterRequest) (*ConnectorCluster, *http.Response, error) {
+	var (
+		localVarHTTPMethod  = http.MethodGet
+		localVarPostBody    interface{}
+		formFiles           []formFile
+		localVarReturnValue *ConnectorCluster
+	)
+
+	localBasePath, err := a.client.cfg.ServerURLWithContext(r.ctx, "ConnectorClustersAdminApiService.GetConnectorCluster")
+	if err != nil {
+		return localVarReturnValue, nil, &GenericOpenAPIError{error: err.Error()}
+	}
+
+	localVarPath := localBasePath + "/api/connector_mgmt/v1/admin/kafka_connector_clusters/{connector_cluster_id}"
+	localVarPath = strings.Replace(localVarPath, "{"+"connector_cluster_id"+"}", url.PathEscape(parameterToString(r.connectorClusterId, "")), -1)
+
+	localVarHeaderParams := make(map[string]string)
+	localVarQueryParams := url.Values{}
+	localVarFormParams := url.Values{}
+
+	// to determine the Content-Type header
+	localVarHTTPContentTypes := []string{}
+
+	// set Content-Type header
+	localVarHTTPContentType := selectHeaderContentType(localVarHTTPContentTypes)
+	if localVarHTTPContentType != "" {
+		localVarHeaderParams["Content-Type"] = localVarHTTPContentType
+	}
+
+	// to determine the Accept header
+	localVarHTTPHeaderAccepts := []string{"application/json"}
+
+	// set Accept header
+	localVarHTTPHeaderAccept := selectHeaderAccept(localVarHTTPHeaderAccepts)
+	if localVarHTTPHeaderAccept != "" {
+		localVarHeaderParams["Accept"] = localVarHTTPHeaderAccept
+	}
+	req, err := a.client.prepareRequest(r.ctx, localVarPath, localVarHTTPMethod, localVarPostBody, localVarHeaderParams, localVarQueryParams, localVarFormParams, formFiles)
+	if err != nil {
+		return localVarReturnValue, nil, err
+	}
+
+	localVarHTTPResponse, err := a.client.callAPI(req)
+	if err != nil || localVarHTTPResponse == nil {
+		return localVarReturnValue, localVarHTTPResponse, err
+	}
+
+	localVarBody, err := ioutil.ReadAll(localVarHTTPResponse.Body)
+	localVarHTTPResponse.Body.Close()
+	localVarHTTPResponse.Body = ioutil.NopCloser(bytes.NewBuffer(localVarBody))
+	if err != nil {
+		return localVarReturnValue, localVarHTTPResponse, err
+	}
+
+	if localVarHTTPResponse.StatusCode >= 300 {
+		newErr := &GenericOpenAPIError{
+			body:  localVarBody,
+			error: localVarHTTPResponse.Status,
+		}
+		if localVarHTTPResponse.StatusCode == 401 {
+			var v Error
+			err = a.client.decode(&v, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+			if err != nil {
+				newErr.error = err.Error()
+				return localVarReturnValue, localVarHTTPResponse, newErr
+			}
+			newErr.model = v
+			return localVarReturnValue, localVarHTTPResponse, newErr
+		}
+		if localVarHTTPResponse.StatusCode == 500 {
+			var v Error
+			err = a.client.decode(&v, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+			if err != nil {
+				newErr.error = err.Error()
+				return localVarReturnValue, localVarHTTPResponse, newErr
+			}
+			newErr.model = v
+		}
+		return localVarReturnValue, localVarHTTPResponse, newErr
+	}
+
+	err = a.client.decode(&localVarReturnValue, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+	if err != nil {
+		newErr := &GenericOpenAPIError{
+			body:  localVarBody,
+			error: err.Error(),
+		}
+		return localVarReturnValue, localVarHTTPResponse, newErr
+	}
+
+	return localVarReturnValue, localVarHTTPResponse, nil
+}
+
 type ApiGetConnectorDeploymentRequest struct {
 	ctx                context.Context
 	ApiService         *ConnectorClustersAdminApiService
--- rhoc/pkg/api/admin/client.go
+++ rhoc/pkg/api/admin/client.go
@@ -52,6 +52,8 @@ type APIClient struct {
 	ConnectorClustersAdminApi *ConnectorClustersAdminApiService
 
 	ConnectorNamespacesAdminApi *ConnectorNamespacesAdminApiService
+
+	ConnectorTypesApi *ConnectorTypesApiService
 }
 
 type service struct {
@@ -72,6 +74,7 @@ func NewAPIClient(cfg *Configuration) *APIClient {
 	// API Services
 	c.ConnectorClustersAdminApi = (*ConnectorClustersAdminApiService)(&c.common)
 	c.ConnectorNamespacesAdminApi = (*ConnectorNamespacesAdminApiService)(&c.common)
+	c.ConnectorTypesApi = (*ConnectorTypesApiService)(&c.common)
 
 	return c
 }

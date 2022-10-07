1ffb90c68f8c5e61b8c6b4fd216b06c699a330d8
Luis Garcia Acosta <lgarciaa@redhat.com>
2022-10-07 16:35:21 +0200

    [chore] add missing tests for critical alerts

--- observability/scripts/rules-check.sh
+++ observability/scripts/rules-check.sh
@@ -9,9 +9,17 @@ CONTAINER_NAME=prometheus-rules-check-rhoc-observability
 docker run -t -p 9090:9090 --name "${CONTAINER_NAME}" \
 	-v "${CONNECTORS_SLO_RULES}":/prometheus/connectors-slo-rules.yaml:z \
 	-v "${CAMELK_OPERATOR_RULES}":/prometheus/camel-k-operator-rules.yaml:z \
+	-v "${FLEETSHARD_CAMEL_OPERATOR_RULES}":/prometheus/cos-fleetshard-operator-camel-rules.yaml:z \
+	-v "${FLEETSHARD_DEBEZIUM_OPERATOR_RULES}":/prometheus/cos-fleetshard-operator-debezium-rules.yaml:z \
+	-v "${FLEETSHARD_SYNC_RULES}":/prometheus/cos-fleetshard-sync-rules.yaml:z \
+	-v "${STRIMZI_OPERATOR_RULES}":/prometheus/strimzi-operator-rules.yaml:z \
 	--entrypoint=/bin/sh \
 	"${IMAGE}" -c '(tail -n +8 connectors-slo-rules.yaml ) > connectors-slo-rules-check.yaml \
   && (tail -n +8 camel-k-operator-rules.yaml) > camel-k-operator-rules-check.yaml \
+  && (tail -n +8 cos-fleetshard-operator-camel-rules.yaml) > cos-fleetshard-operator-camel-rules-check.yaml \
+  && (tail -n +8 cos-fleetshard-operator-debezium-rules.yaml) > cos-fleetshard-operator-debezium-rules-check.yaml \
+  && (tail -n +8 cos-fleetshard-sync-rules.yaml) > cos-fleetshard-sync-rules-check.yaml \
+  && (tail -n +8 strimzi-operator-rules.yaml) > strimzi-operator-rules-check.yaml \
 	&& promtool check rules *-rules-check.yaml' \
 	docker stop "${CONTAINER_NAME}"
 

c0e45bd70e7c36af9fdd4e7ad767b0f1d9da333e
Luis Garcia Acosta <lgarciaa@redhat.com>
2022-11-21 17:34:20 +0100

    feat: allow a connector to be stopped or started
    Introduce `stop` and `start` subcommands for connectors

--- rhoc/pkg/cmd/connectors/connectors.go
+++ rhoc/pkg/cmd/connectors/connectors.go
@@ -4,6 +4,7 @@ import (
 	"github.com/bf2fc6cc711aee1a0c2a/cos-tools/rhoc/pkg/cmd/connectors/delete"
 	"github.com/bf2fc6cc711aee1a0c2a/cos-tools/rhoc/pkg/cmd/connectors/get"
 	"github.com/bf2fc6cc711aee1a0c2a/cos-tools/rhoc/pkg/cmd/connectors/list"
+	"github.com/bf2fc6cc711aee1a0c2a/cos-tools/rhoc/pkg/cmd/connectors/patch"
 	"github.com/bf2fc6cc711aee1a0c2a/cos-tools/rhoc/pkg/cmd/connectors/types"
 	"github.com/bf2fc6cc711aee1a0c2a/cos-tools/rhoc/pkg/util/cmdutil"
 	"github.com/redhat-developer/app-services-cli/pkg/shared/factory"
@@ -22,7 +23,9 @@ func NewConnectorsCommand(f *factory.Factory) *cobra.Command {
 		list.NewListCommand(f),
 		delete.NewDeletesCommand(f),
 		get.NewGetCommand(f),
-		types.NeConnectorTypesCommand(f))
+		types.NeConnectorTypesCommand(f),
+		patch.NewReadyCommand(f),
+		patch.NewStopCommand(f))
 
 	return cmd
 }
---
+++ rhoc/pkg/cmd/connectors/patch/ready.go
@@ -0,0 +1,72 @@
+package patch
+
+import (
+	"github.com/bf2fc6cc711aee1a0c2a/cos-tools/rhoc/pkg/api/admin"
+	"github.com/bf2fc6cc711aee1a0c2a/cos-tools/rhoc/pkg/service"
+	"github.com/bf2fc6cc711aee1a0c2a/cos-tools/rhoc/pkg/util/cmdutil"
+	"github.com/bf2fc6cc711aee1a0c2a/cos-tools/rhoc/pkg/util/response"
+	"github.com/redhat-developer/app-services-cli/pkg/core/ioutil/dump"
+	"github.com/redhat-developer/app-services-cli/pkg/shared/factory"
+	"github.com/spf13/cobra"
+)
+
+func NewReadyCommand(f *factory.Factory) *cobra.Command {
+	opts := options{
+		f: f,
+	}
+
+	cmd := &cobra.Command{
+		Use: "start",
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			if err := cmdutil.ValidateOutputs(cmd); err != nil {
+				return err
+			}
+
+			return nil
+		},
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return opts.ready()
+		},
+	}
+
+	cmdutil.AddOutput(cmd, &opts.outputFormat)
+	cmdutil.AddYes(cmd, &opts.skipConfirm)
+	cmdutil.AddID(cmd, &opts.id).Required()
+
+	return cmd
+}
+
+func (opts options) ready() error {
+	if !opts.skipConfirm {
+		confirm, promptErr := cmdutil.PromptConfirm("Are you sure you want to start the connector with id '%s'?", opts.id)
+		if promptErr != nil {
+			return promptErr
+		}
+		if !confirm {
+			opts.f.Logger.Debug("User has chosen to not start connector")
+			return nil
+		}
+	}
+
+	c, err := service.NewAdminClient(&service.Config{
+		F: opts.f,
+	})
+	if err != nil {
+		return err
+	}
+
+	reqBody := map[string]interface{}{
+		"desired_state": admin.CONNECTORSTATE_READY,
+	}
+
+	res, resp, err := c.Clusters().PatchConnector(opts.f.Context, opts.id).Body(reqBody).Execute()
+	if err != nil {
+		return response.Error(err, resp)
+	}
+
+	if resp != nil && resp.StatusCode > 299 {
+		return dump.Formatted(opts.f.IOStreams.Out, opts.outputFormat, res)
+	}
+
+	return nil
+}
